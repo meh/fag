c1bd9462157ad9bed7d9529fa81d1b351d79fcae
meh <meh@paranoici.org>
2012-02-02 19:56:14 +0100

    cleanup: merge extensions.rb with environment.rb

--- environment.rb
+++ environment.rb
@@ -1,3 +1,13 @@
+#--
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#                    Version 2, December 2004
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#
+#  0. You just DO WHAT THE FUCK YOU WANT TO.
+#++
+
 require 'rubygems'
 require 'bundler'
 require 'rack'
@@ -5,8 +15,50 @@ require 'rack/csrf'
 
 Bundler.setup :default, (ENV['RACK_ENV'] || 'development')
 
-# load various dependencies and stuff
-require 'extensions'
+# some dependencies and extensions
+require 'json'
+require 'boolean/expression'
+
+module Fag
+
+module Serializable
+	def self.included (klass)
+		class << klass
+			def serialize_as (&block)
+				define_method :serializable_hash, &block
+				define_method :to_hash, &block
+			end
+		end
+	end
+end
+
+module Authored
+	def self.included (klass)
+		klass.instance_eval {
+			property :author_name, DataMapper::Property::String, required: false
+			property :author_id,  DataMapper::Property::Integer, required: false
+
+			property :created_at, DataMapper::Property::DateTime
+			property :updated_at, DataMapper::Property::DateTime
+		}
+	end
+
+	def anonymous?; author_id.nil?; end
+
+	def author
+		anonymous? ? Anonymous.new(author_name) : User.get(author_id)
+	end
+end
+
+module Versioned
+	def self.included (klass)
+		klass.instance_eval {
+			is_versioned on: :updated_at
+		}
+	end
+end
+
+end
 
 # database stuff
 require 'data_mapper'
